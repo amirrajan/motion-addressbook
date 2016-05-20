d859adef9c01ecbe5c14043f219e865857f573d1
Jonathan Bender <jlbender@gmail.com>
2016-05-20 10:11:46 -0400

    convert all CN accessors to async

--- motion/address_book/cn/models/contact_store.rb
+++ motion/address_book/cn/models/contact_store.rb
@@ -45,90 +45,133 @@ module AddressBook
 
       # Groups
 
-      def groups
-        ensure_connection! do
-          Accessors::Groups.index(@native_ref).map { |group| group_new(group) }
-        end
+      def groups(&callback)
+        with_connection(:connected_groups, &callback)
       end
 
-      def groups_count
-        ensure_connection! { Accessors::Groups.count(@native_ref) }
+      def groups_count(&callback)
+        with_connection(:connected_groups_count, &callback)
       end
 
-      def group_new(attributes)
-        ensure_connection! { Group.new(attributes, @native_ref) }
+      def group_new(attributes, &callback)
+        with_connection(:connected_groups_new, attributes, &callback)
       end
 
-      def group_create(attributes)
-        ensure_connection! { group_new(attributes).save }
+      def group_create(attributes, &callback)
+        with_connection(:connected_groups_create, attributes, &callback)
       end
 
-      def group_find(id)
-        ensure_connection! do
-          ab_group = Accessors::Groups.find(@native_ref, id)
-          ab_group && group_new(ab_group)
-        end
+      def group_find(id, &callback)
+        with_connection(:connected_groups_find, id, &callback)
       end
 
       # People
 
-      def contacts(options = {})
-        ensure_connection! do
-          Accessors::Contacts.index(@native_ref)
-            .map { |cn_contact| contact_new(cn_contact) }
-        end
+      def contacts(options = {}, &callback)
+        with_connection(:connected_contacts, options, &callback)
       end
       alias :people :contacts
 
-      def contacts_changed_since(timestamp)
-        ensure_connection! do
-          Accessors::Contacts.changed_since(@native_ref, timestamp)
-        end
+      def contacts_changed_since(timestamp, &callback)
+        with_connection(:connected_contacts_changed_since, timestamp, &callback)
       end
       alias :people_changed_since :contacts_changed_since
 
-      def contacts_count
-        ensure_connection! { Accessors::Contacts.count(@native_ref) }
+      def contacts_count(&callback)
+        with_connection(:connected_contacts_count, &callback)
       end
       alias :people_count :contacts_count
 
-      def contacts_where(conditions)
-        ensure_connection! do
-          contacts.select { |person| person.matches? conditions }
-        end
+      def contacts_where(conditions, &callback)
+        with_connection(:connected_contacts_where, conditions, &callback)
       end
       alias :people_where :contacts_where
 
-      def contact_new(attributes)
-        ensure_connection! { Contact.new(attributes) }
+      def contact_new(attributes, &callback)
+        with_connection(:connected_contacts_new, attributes, &callback)
       end
       alias :person_new :contact_new
 
-      def contact_create(attributes)
-        ensure_connection! { contact_new(attributes).save }
+      def contact_create(attributes, &callback)
+        with_connection(:connected_contacts_create, attributes, &callback)
       end
       alias :person_create :contact_create
 
-      def contact_find(id)
-        ensure_connection! do
-          cn_contact = Accessors::Contacts.find(@native_ref, id)
-          cn_contact && contact_new(cn_contact)
-        end
+      def contact_find(id, &callback)
+        with_connection(:connected_contacts_find, id, &callback)
       end
       alias :person_find :contact_find
 
       # Sources
 
-      def sources
-        ensure_connection! do
-          Accessors::Sources.index(@native_ref).map { |s| Source.new(s) }
-        end
+      def sources(&callback)
+        with_connection(:connected_sources_index, &callback)
       end
 
       private
 
+      def connected_contacts(options)
+        Accessors::Contacts.index(@native_ref)
+          .map { |cn_contact| connected_contacts_new(cn_contact) }
+      end
+
+      def connected_contacts_changed_since(timestamp)
+        Accessors::Contacts.changed_since(@native_ref, timestamp)
+      end
+
+      def connected_contacts_count
+        Accessors::Contacts.count(@native_ref)
+      end
+
+      def connected_contacts_create(attributes)
+        connected_contacts_new(attributes).save
+      end
+
+      def connected_contacts_new(attributes)
+        Contact.new(attributes)
+      end
+
+      def connected_contacts_find(id)
+        cn_contact = Accessors::Contacts.find(@native_ref, id)
+        cn_contact && connected_contacts_new(cn_contact)
+      end
+
+      def connected_contacts_where(conditions)
+        connected_contacts.select { |person| person.matches? conditions }
+      end
+
+      def connected_groups
+        Accessors::Groups.index(@native_ref).map { |group| group_new(group) }
+      end
+
+      def connected_groups_count
+        Accessors::Groups.count(@native_ref)
+      end
+
+      def connected_groups_new(attributes)
+        Group.new(attributes, @native_ref)
+      end
+
+      def connected_groups_create(attributes)
+        group_new(attributes).save
+      end
+
+      def connected_groups_find(id)
+        ab_group = Accessors::Groups.find(@native_ref, id)
+        ab_group && group_new(ab_group)
+      end
+
+      def connected_sources_index
+        Accessors::Sources.index(@native_ref)
+          .map { |source| Source.new(source) }
+      end
+
       def ensure_connection!(&after_connect)
         connected? ? after_connect.call : connect(&after_connect)
+      end
+
+      def with_connection(method_name, *args, &callback)
+        ensure_connection! { callback.call(send(method_name, *args)) }
       end
     end
   end
